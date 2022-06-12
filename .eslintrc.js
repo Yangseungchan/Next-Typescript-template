e85d3ddb3e7d3d27d45b6acb9d19b06b5076f72b
Yangseungchan <uvzone@naver.com>
2022-06-12 15:58:18 +0900

    [Add] Husky setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,20 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-    "plugin:react/recommended",
-    "google",
-    "next/core-web-vitals",
-    "prettier", 
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
+  extends: ["plugin:react/recommended", "google", "next/core-web-vitals", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": "latest",
-    "sourceType": "module",
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint",
-  ],
-  "rules": {
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
     "require-jsdoc": "off",
-    "quotes": ["error", "double"], // 문자열 들을 쌍따옴표로 감싸도록 강제
+    quotes: ["error", "double"], // 문자열 들을 쌍따옴표로 감싸도록 강제
   },
 };
