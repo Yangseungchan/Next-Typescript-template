bbe6f18d85544ea11409d8138642c370986e3b4e
Yangseungchan <uvzone@naver.com>
2022-06-15 22:02:44 +0900

    [Edit] 폴더구조 제대로 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Title from "@components/Title";
 import { Container } from "@styles/home";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Title title="NextJS TypeScript Template" />
+      <Image src="/vercel.svg" alt="vercel" width="100px" height="100px" />
     </Container>
   );
 }
